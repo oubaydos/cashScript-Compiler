59e150261603356fe9b55d6f2bcb75e16b3acc9c
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-01-31 10:05:07 +0100

    pas de double declaration rule is verified

--- lexer/main.c
+++ lexer/main.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
   //   return -1;
   // }else{
   //   fp = fopen(argv[1],"r");
-  fp = fopen("../tests/test1.cash", "r"); //for debuging
+  fp = fopen("../tests/test1.cash", "r"); // for debuging
   if (fp == NULL)
   {
     perror("Error in opening file");
@@ -25,8 +25,9 @@ int main(int argc, char *argv[])
   printf("BRAVO: le programme est correcte!!!");
   // you should assert that the expected tokens in the file ../tests/expected1.txt are the same as the result tokens of analysing test1.txt
   fclose(fp);
+  // printf("--\n%d\n", pascal_keywords[0].token);
   return 0;
   //}
 }
 
-//cd "C:\Users\hp\Desktop\Pascal-master1\Pascal-master\src" && C:\TDM-GCC-64\bin\gcc main.c -o main && "C:\Users\hp\Desktop\Pascal-master1\Pascal-master\src"\main
+// cd "C:\Users\hp\Desktop\Pascal-master1\Pascal-master\src" && C:\TDM-GCC-64\bin\gcc main.c -o main && "C:\Users\hp\Desktop\Pascal-master1\Pascal-master\src"\main
